ff413de2178a9c9c5fee29a5b1b015cae4453aea
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-02-14 19:28:23 -0800

    Modify mutation for adding clients

--- client/src/mutations/clientMutations.js
+++ client/src/mutations/clientMutations.js
@@ -11,8 +11,10 @@ export const DELETE_CLIENT = gql`
     }
 `
 export const ADD_CLIENT = gql`
-    mutation addClient {
-        addProject {
+    mutation addClient($name: String!, $email: String!, $phone: String!) 
+    {
+        addClient(name: $name, email: $email, phone: $phone) 
+        {
             id
             name
             email
