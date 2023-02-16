7561c149ce43082962ba3eeb3cc64ed05e4ef8cd
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-02-15 19:00:01 -0800

    Imports

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,13 @@
+import Clients from "../components/Clients";
+import AddClientModal from "../components/AddClientModal";
+import Projects from "../components/Projects";
+
 export default function Home() {
   return (
     <>
-    
+        <AddClientModal/>
+        <Projects/>
+        <Clients/>
     </>
   )
 }
