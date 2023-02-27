7f081a46e706753f9ea0d8f892c7aca96f29aae2
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-02-26 21:37:33 -0800

    Clean

--- server/index.js
+++ server/index.js
@@ -32,11 +32,4 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-// if (process.env.NODE_ENV === 'production') {
-
-//   app.use(express.static('client/build'));
-
-//   app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-// }
-
 app.listen(port, console.log(`Server running on port ${port}`));
